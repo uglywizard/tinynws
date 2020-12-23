21ed595d8b36da6d7b545fc2cae52e06779a56e0
Ugly Wiz <uglywizard@protonmail.com>
2020-12-23 03:02:00 +0100

    Trying hacky method.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn neod_backend_api.wsgi --log-file -
+web:  python project/manage.py collectstatic --noinput; gunicorn neod_backend_api.wsgi --log-file -
--- neod_backend_api/wsgi.py
+++ neod_backend_api/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neod_backend_api.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ("DJANO_SETTINGS_MODULE"))
 
 application = get_wsgi_application()
