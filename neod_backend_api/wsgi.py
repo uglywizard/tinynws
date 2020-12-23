1c90c87bc40ea5ba82243552a677b1179eee8878
Ugly Wiz <uglywizard@protonmail.com>
2020-12-23 03:08:23 +0100

    Trying hacky method.

--- neod_backend_api/wsgi.py
+++ neod_backend_api/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ("DJANO_SETTINGS_MODULE"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noed_backend_api.settings.prod")
 
 application = get_wsgi_application()
