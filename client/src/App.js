c1193cb688f126fec6c8403e49049336f3fbae8c
uglywizard <31808528+uglywizard@users.noreply.github.com>
2020-12-24 13:59:24 +0100

    Comments.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,13 @@ import Paginator from "./containers/Paginator";
 import Headings from "./components/Headings";
 import Footer from "./components/Footer";
 
+/*
+Given the simplicity of the requests, I decided to not use axios.
+The two distinct requests are purely for the purpose of testing both endpoints of the API.
+In retrospect I could have implemented a central store to manage
+the data flow uniformly on both components.
+*/
+
 function App() {
   return (
     <div className='main'>
